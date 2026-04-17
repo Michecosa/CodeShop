5238ec91be86c2687032fa76c3b14f124b78d16c
DonatoMorra <donatomorra90@gmail.com>
2026-04-17 11:02:42 +0200

    .

--- final_project/src/main/java/com/example/final_project/Repository/ItemRepository.java
+++ final_project/src/main/java/com/example/final_project/Repository/ItemRepository.java
@@ -1,8 +1,4 @@
 package com.example.final_project.Repository;
 
-import com.generation.ammazzon.model.entities.ItemAmmazzon;
-import com.generation.ammazzon.model.entities.Prodotto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemDao extends JpaRepository<ItemAmmazzon, Long> {
+public interface ItemRepository extends JpaRepository<ItemAmmazzon, Long> {
 }
--- final_project/src/main/java/com/example/final_project/Repository/ProdottoRepository.java
+++ final_project/src/main/java/com/example/final_project/Repository/ProdottoRepository.java
@@ -1,8 +1,4 @@
 package com.example.final_project.Repository;
 
-import com.generation.ammazzon.model.entities.Carrello;
-import com.generation.ammazzon.model.entities.Prodotto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdottoDao extends JpaRepository<Prodotto, Long> {
+public interface ProdottoRepository extends JpaRepository<Prodotto, Long> {
 }
