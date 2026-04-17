6639dd8299d7f188b57b90334cf33c9261deb09f
DonatoMorra <donatomorra90@gmail.com>
2026-04-17 12:15:44 +0200

    Update item service

--- final_project/src/main/java/com/example/final_project/Service/ItemService.java
+++ final_project/src/main/java/com/example/final_project/Service/ItemService.java
@@ -1,8 +1,41 @@
 package com.example.final_project.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.final_project.Model.ItemQuantity;
+import com.example.final_project.Repository.ItemRepository;
+
+import java.util.List;
 
 @Service
 public class ItemService {
 
+    @Autowired
+    ItemRepository itemRepository;
+
+    public List<ItemQuantity> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public ItemQuantity getItemById(Long idItem) {
+        return itemRepository.findById(idItem)
+                .orElseThrow(() -> new RuntimeException("Item non trovato con id: " + idItem));
+    }
+
+    // Mancava: creare un nuovo item
+    public ItemQuantity createItem(ItemQuantity item) {
+        return itemRepository.save(item);
+    }
+
+    public ItemQuantity updateQtn(Long idItem, int qtn) {
+        ItemQuantity item = itemRepository.findById(idItem)
+                .orElseThrow(() -> new RuntimeException("Item non trovato con id: " + idItem));
+        item.setQtn(qtn);
+        return itemRepository.save(item);
+    }
+
+    public void deleteItem(Long idItem) {
+        itemRepository.deleteById(idItem);
+    }
 }
