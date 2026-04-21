66283ed649a8fef9ed98b6ff9e228b4751f6df37
DonatoMorra <donatomorra90@gmail.com>
2026-04-21 09:10:46 +0200

    Update AuthController.java

--- final_project/src/main/java/com/example/final_project/Controller/AuthController.java
+++ final_project/src/main/java/com/example/final_project/Controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.example.final_project.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
